1d0a62c42f470368e90589681c8108442266e01b
David Breckx <david.breckx@telenet.be>
2024-10-21 12:20:19 +0200

    Movie styling

--- react-bios/src/components/Movie.jsx
+++ react-bios/src/components/Movie.jsx
@@ -5,13 +5,16 @@ import duneImg from "../assets/dune_poster.jpg";
 
 const Movie = ({ movie }) => {
   return (
-    <div>
+    <div className="shadow-lg rounded-xl overflow-clip">
       {/* Statische image */}
       {/* <img src={duneImg} /> */}
       <img
         src={new URL(`../assets/${movie.poster_path}`, import.meta.url).href}
       />
-      <p>{movie.title}</p>
+      <div className=" min-h-36">
+        <p className="font-bold text-center">{movie.title}</p>
+        <p>{movie.genres.join(", ")}</p>
+      </div>
     </div>
   );
 };
